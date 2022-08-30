eb6134665aa82e7d36740071ac224a7f841aef44
chp <chenhaipeng@maxwell-gp.com.cn>
2022-08-30 21:39:00 +0800

    状态机模板

---
+++ StateMachinePattern/StateMachinePattern/StateMachinePattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+
+namespace StateMachinePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StateMachine sm = new StateMachine();
+            StateA s = new StateA(sm);
+            sm.Current = s;
+            while(true)
+            {
+                sm.Execute();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ StateMachinePattern/StateMachinePattern/StateMachinePattern/StateMachine.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace StateMachinePattern
+{
+    public interface IState
+    {
+        void Execute();
+    }
+
+    public class StateA : IState
+    {
+        private StateMachine _sm;
+
+        public StateA(StateMachine sm)
+        {
+            _sm = sm;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("State A");
+            _sm.Current = new StateB(_sm);
+        }
+    }
+
+    public class StateB : IState
+    {
+        private StateMachine _sm;
+
+        public StateB(StateMachine sm)
+        {
+            _sm = sm;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("State B");
+            _sm.Current = new StateA(_sm);
+        }
+    }
+
+    public class StateMachine
+    {
+        public IState Current { get; set; }
+
+        public void Execute()
+        {
+            Current.Execute();
+        }
+    }
+}
