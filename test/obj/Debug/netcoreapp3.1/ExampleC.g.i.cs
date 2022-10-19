18094d9a41bfeff96a604a29802810511d4cff41
luochenchen <1225300556@qq.com>
2022-10-19 19:49:04 +0800

    111

--- test/obj/Debug/netcoreapp3.1/ExampleC.g.i.cs
+++ test/obj/Debug/netcoreapp3.1/ExampleC.g.i.cs
@@ -53,7 +53,7 @@ namespace test {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/test;V1.0.0.0;component/examplec.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/test;component/examplec.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\ExampleC.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
