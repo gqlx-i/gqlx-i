4964ea15478ded454b0634d24884e27bcd971b91
chp <chenhaipeng@maxwell-gp.com.cn>
2022-10-25 17:21:04 +0800

    1

---
+++ test/ViewModel/ScatterPlotWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test.ViewModel
+{
+    public class ScatterPlotWindowViewModel
+    {
+        public double UpLimit { get; set; } = 15;
+        public double LowLimit { get; set; } = -15;
+        public double MidLine { get; set; }
+    }
+}
